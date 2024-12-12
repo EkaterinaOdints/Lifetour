5529fda33a878868c2d617a8ce5478c9d5e736df
EkaterinaOdints <157913603+EkaterinaOdints@users.noreply.github.com>
2024-12-12 18:19:58 +0300

    Добавляет разметку и стилизацию хедера

--- source/js/main.js
+++ source/js/main.js
@@ -2,3 +2,14 @@
 // import Swiper from "swiper";
 // import {Navigation, Pagination} from "swiper/modules";
 // import 'swiper/css';
+
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+const swiper = new Swiper('.swiper', {
+  modules: [Navigation, Pagination],
+});
+
+
