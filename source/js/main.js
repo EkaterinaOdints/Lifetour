1f59636adbf6eb68118649403e7bb1441cd9c72b
EkaterinaOdints <157913603+EkaterinaOdints@users.noreply.github.com>
2024-12-13 19:48:48 +0300

    Оживляет меню

--- source/js/main.js
+++ source/js/main.js
@@ -3,6 +3,33 @@
 // import {Navigation, Pagination} from "swiper/modules";
 // import 'swiper/css';
 
+const body = document.querySelector('body');
+const navigation = body.querySelector('.main-navigation-js');
+const navButton = navigation.querySelector('.main-navigation-button-js');
+
+const closeMenu = () => {
+  body.classList.remove('overlay');
+  navigation.classList.remove('main-navigation--oppened');
+};
+
+const onLinkClick = (e) => {
+  if (e.target.tagName === 'A') {
+    closeMenu();
+  }
+};
+
+const toggleMenu = () => {
+  body.classList.toggle('overlay');
+  navigation.classList.toggle('main-navigation--oppened');
+
+  if (body.classList.contains('overlay') || navigation.classList.contains('main-navigation--oppened')) {
+    navigation.removeEventListener('click', onLinkClick);
+    navigation.addEventListener('click', onLinkClick);
+  }
+};
+
+navButton.addEventListener('click', toggleMenu);
+
 import Swiper from 'swiper';
 import { Navigation, Pagination } from 'swiper/modules';
 import 'swiper/css';
