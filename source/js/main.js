61c2d3deed598a764ea91a82153099a1e27e7392
EkaterinaOdints <157913603+EkaterinaOdints@users.noreply.github.com>
2024-12-23 13:24:13 +0300

    Добавляет блоки Промо и Ближайшие туры

--- source/js/main.js
+++ source/js/main.js
@@ -1,42 +1,71 @@
-// https://swiperjs.com/get-started#installation
-// import Swiper from "swiper";
-// import {Navigation, Pagination} from "swiper/modules";
-// import 'swiper/css';
+const useMenu = () => {
+  const body = document.querySelector('body');
+  const navigation = body.querySelector('.main-navigation-js');
+  const navButton = navigation.querySelector('.main-navigation-button-js');
 
-const body = document.querySelector('body');
-const navigation = body.querySelector('.main-navigation-js');
-const navButton = navigation.querySelector('.main-navigation-button-js');
+  const closeMenu = () => {
+    body.classList.remove('overlay');
+    navigation.classList.remove('main-navigation--oppened');
+  };
 
-const closeMenu = () => {
-  body.classList.remove('overlay');
-  navigation.classList.remove('main-navigation--oppened');
+  const onLinkClick = (e) => {
+    if (e.target.tagName === 'A') {
+      closeMenu();
+    }
+  };
+
+  const toggleMenu = () => {
+    body.classList.toggle('overlay');
+    navigation.classList.toggle('main-navigation--oppened');
+
+    if (body.classList.contains('overlay') || navigation.classList.contains('main-navigation--oppened')) {
+      navigation.removeEventListener('click', onLinkClick);
+      navigation.addEventListener('click', onLinkClick);
+    }
+  };
+
+  navButton.addEventListener('click', toggleMenu);
 };
 
-const onLinkClick = (e) => {
-  if (e.target.tagName === 'A') {
-    closeMenu();
-  }
-};
+useMenu();
 
-const toggleMenu = () => {
-  body.classList.toggle('overlay');
-  navigation.classList.toggle('main-navigation--oppened');
-
-  if (body.classList.contains('overlay') || navigation.classList.contains('main-navigation--oppened')) {
-    navigation.removeEventListener('click', onLinkClick);
-    navigation.addEventListener('click', onLinkClick);
-  }
-};
-
-navButton.addEventListener('click', toggleMenu);
 
 import Swiper from 'swiper';
 import { Navigation, Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-const swiper = new Swiper('.swiper', {
-  modules: [Navigation, Pagination],
+
+const promoSwiper = new Swiper('.promo-swiper-js', {
+  modules: [Pagination],
+  direction: 'horizontal',
+  pagination: {
+    el: '.promo-swiper-pagination-js',
+    clickable: true,
+  },
+  loop: true,
+});
+
+const toursSwiper = new Swiper('.tours-swiper-js', {
+  modules: [Navigation],
+  direction: 'horizontal',
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  loop: true,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 18,
+      loop: false,
+    },
+    1440: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      loop: false,
+    }
+  }
 });
 
 
